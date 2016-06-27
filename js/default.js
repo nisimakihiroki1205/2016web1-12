279186b80a2a6644d5a97ff9860e05ebddb6ec88
nisimakihiroki1205 <g1527055@st.t-kougei.ac.jp>
2016-06-27 17:43:49 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 var i = Math.floor(0);
 var input = Math.floor(100);
 var output;
-document.write("<p>名前:" + name + "</p>");
+
 var output=0;// = parseInt(window.prompt('正の数字を入力して下さい。'));
 
 var message;
@@ -11,13 +11,13 @@ for(i=0;i===input;i++){
     output[0]=0
   }
   
-  if(input[i]%3===0){
+  if(input[i]%3==0){
     output[i]=Fizz;
   }
-  if(input[i]%5===0){
+  if(input[i]%5==0){
     output[i]=Buzz;
   }
-  if(input[i]%3===0&&input[i]%5===0){
+  if(input[i]%3==0&&input[i]%5==0){
     output[i]=FizzBuzz;
   }
   document.write("<p>"+i+'は'+output[i]+"</p>");
